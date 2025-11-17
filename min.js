a06cc40a35fbbb7d53af7231159501b8afc8221c
hai07t <hai07t@gmail.com>
2025-11-17 16:17:17 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,6 +1,6 @@
-const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+// const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-if (userTimeZone === "Asia/Ho_Chi_Minh" || userTimeZone === "Asia/Saigon") {
-  var to = "https://github.com/KateIvesDev/QueenBee";
-  window.location.href = to;
-}
+// if (userTimeZone === "Asia/Ho_Chi_Minh" || userTimeZone === "Asia/Saigon") {
+//   var to = "https://github.com/KateIvesDev/QueenBee";
+//   window.location.href = to;
+// }
